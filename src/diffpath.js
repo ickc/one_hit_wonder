f8db3c2de7ab8103b1108b337c4ddea419509cb5
Kolen Cheung <christian.kolen@gmail.com>
2024-06-30 00:38:48 +0100

    Update diffpath.js

--- src/diffpath.js
+++ src/diffpath.js
@@ -4,65 +4,59 @@ const fs = require("fs");
 const path = require("path");
 
 // Function to get all executable files in a directory
-function getExecutableFiles(dir) {
-  let executables = new Set();
-  try {
-    const files = fs.readdirSync(dir);
-    files.forEach((file) => {
-      const filePath = path.join(dir, file);
-      try {
-        const stats = fs.statSync(filePath);
-        if (stats.isFile() && stats.mode & 0o111) {
-          // Check any execute permission
-          executables.add(file);
-        }
-      } catch (err) {
-        // Handle errors related to file stat
-      }
-    });
-  } catch (err) {
-    // Handle errors related to directory reading
-  }
+function getExecutableFiles(paths) {
+  const executables = new Set();
+  paths.split(":").forEach((dir) => {
+    try {
+      const files = fs.readdirSync(dir);
+      files.forEach((file) => {
+        const filePath = path.join(dir, file);
+        try {
+          const stats = fs.statSync(filePath);
+          if (stats.isFile() && stats.mode & 0o111) {
+            // Check any execute permission
+            executables.add(file);
+          }
+        } catch (err) {}
+      });
+    } catch (err) {}
+  });
   return executables;
 }
 
-// Function to get the difference between two sets of executables
-function diffExecutables(set1, set2) {
-  const onlyInSet1 = [...set1].filter((x) => !set2.has(x));
-  const onlyInSet2 = [...set2].filter((x) => !set1.has(x));
-  return { onlyInSet1, onlyInSet2 };
+// Function to get the symmetric difference between two sets
+function symmetricDifferenceList(set1, set2) {
+  const difference = new Array();
+  set1.forEach((value) => {
+    if (!set2.has(value)) {
+      difference.push(value);
+    }
+  });
+  set2.forEach((value) => {
+    if (!set1.has(value)) {
+      difference.push(value);
+    }
+  });
+  difference.sort();
+  return difference;
 }
 
 // Main function to execute the diffpath logic
 function main() {
-  const [, , PATH1, PATH2] = process.argv;
-  if (!PATH1 || !PATH2) {
-    console.error("Usage: diffpath PATH1 PATH2");
+  const [, , path1, path2] = process.argv;
+  if (!path1 || !path2) {
+    const programName = path.basename(process.argv[1]);
+    console.error(`Usage: ${programName} PATH1 PATH2`);
     process.exit(1);
   }
 
-  const path1Dirs = PATH1.split(":");
-  const path2Dirs = PATH2.split(":");
+  const execSet1 = getExecutableFiles(path1);
+  const execSet2 = getExecutableFiles(path2);
 
-  let execSet1 = new Set();
-  let execSet2 = new Set();
+  const sortedDiff = symmetricDifferenceList(execSet1, execSet2);
 
-  path1Dirs.forEach((dir) => {
-    const execFiles = getExecutableFiles(dir);
-    execFiles.forEach((file) => execSet1.add(file));
-  });
-
-  path2Dirs.forEach((dir) => {
-    const execFiles = getExecutableFiles(dir);
-    execFiles.forEach((file) => execSet2.add(file));
-  });
-
-  const { onlyInSet1, onlyInSet2 } = diffExecutables(execSet1, execSet2);
-
-  const combinedSorted = [...onlyInSet1, ...onlyInSet2].sort();
-
-  combinedSorted.forEach((file) => {
-    if (onlyInSet1.includes(file)) {
+  sortedDiff.forEach((file) => {
+    if (execSet1.has(file)) {
       console.log(file);
     } else {
       console.log(`\t${file}`);
