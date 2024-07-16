1ca91f93387a12937f2b5f5089b8e8e28a3f5f91
Kolen Cheung <christian.kolen@gmail.com>
2024-07-16 20:21:24 +0100

    Revert "build nuitka on macOS 13 too"
    This reverts commit 92f297b3b220ff976ce663df8650b8010e765929.

--- util/env.sh
+++ util/env.sh
@@ -76,8 +76,10 @@ case "${METHOD}" in
         get_command_path CYTHONIZE envs/cython cythonize
         get_command_path CYTHON_PYTHON envs/cython python
 
-        get_command_path NUITKA_PYTHON envs/nuitka python
-        get_env_var NUITKA_PYTHONPATH envs/nuitka PYTHONPATH
+        if [[ "$(uname -s)" != Darwin || $(sw_vers -productVersion | awk -F '.' '{print $1}') -ge 14 ]]; then
+            get_command_path NUITKA_PYTHON envs/nuitka python
+            get_env_var NUITKA_PYTHONPATH envs/nuitka PYTHONPATH
+        fi
         ;;
     *) ;;
 esac
