bdb998e460dc3821245ee768ac8408071d95aacf
Kolen Cheung <christian.kolen@gmail.com>
2024-07-29 07:52:21 +0100

    Create gitignored.sh

---
+++ src/gitignored.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# getopts ######################################################################
+
+usage="Usage: ${0} [-e] [-d] [directory]
+List all git-ignored files under the given directory.
+    -e  List files in a git-ignored directory.
+    -d  Verify paths exist, print to stderr if not. (Not implemented.)
+"
+
+directory="."
+expand_directory=""
+debug=false
+
+while getopts "ed" opt; do
+    case ${opt} in
+        e)
+            expand_directory="--untracked-files=all"
+            ;;
+        d)
+            # shellcheck disable=SC2034
+            debug=true
+            ;;
+        *)
+            echo "${usage}"
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND - 1))
+
+if [[ $# -gt 0 ]]; then
+    directory=${1}
+fi
+
+# shellcheck disable=SC2312
+find "${directory}" -name .git -exec bash -c '
+	for git do
+		dir="${git::-4}"
+		{
+            cd "${dir}"
+            git status . --ignored --ignore-submodules=all --no-renames --porcelain=1 -z '"${expand_directory}"' | awk -v dir="${dir}" -v RS="\0" -v ORS="\0" "/^!! / {print dir substr(\$0,4)}"
+        }
+	done' _ {} + | sort -z | tr '\0' '\n'
